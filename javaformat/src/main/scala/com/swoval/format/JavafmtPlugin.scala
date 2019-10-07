9b407b325a48c3963332361a756462d7e7a7a0e5
Ethan Atkins <contact@ethanatkins.com>
2019-10-06 18:40:07 -0700

    Add keys to SourceFormatPlugin
    This makes it easier to use the plugin in a scala source file as opposed
    to a build.sbt.

--- javaformat/src/main/scala/com/swoval/format/JavafmtPlugin.scala
+++ javaformat/src/main/scala/com/swoval/format/JavafmtPlugin.scala
@@ -4,20 +4,20 @@ import com.swoval.format.java.JavaFormatter
 import com.swoval.format.lib.SourceFormat
 import sbt._
 
-object JavafmtPlugin extends AutoPlugin {
+object JavafmtPlugin extends AutoPlugin with JavafmtKeys {
   override def trigger = allRequirements
-  trait Keys {
-    val javafmt = taskKey[Unit]("Format source files using the google java formatter.")
-    val javafmtCheck =
-      taskKey[Unit]("Check source file formatting using the google java formatter.")
-  }
-  object autoImport extends Keys
+  object autoImport extends JavafmtKeys
 
-  import autoImport._
   override lazy val projectSettings = SourceFormat.settings(
-    javafmt,
+    autoImport.javafmt,
     JavaFormatter,
     SourceFormat.compileSources(Compile, "*.java"),
     SourceFormat.compileSources(Test, "*.java"),
   )
+}
+
+private[format] trait JavafmtKeys {
+  val javafmt = taskKey[Unit]("Format source files using the google java formatter.")
+  val javafmtCheck =
+    taskKey[Unit]("Check source file formatting using the google java formatter.")
 }
