17d57b018414203ff1b0af45688c93583ecf4c5b
Ethan Atkins <contact@ethanatkins.com>
2019-10-08 10:40:38 -0700

    Add support for custom formatters

--- javaformat/src/main/scala/com/swoval/format/java/JavaFormatter.scala
+++ javaformat/src/main/scala/com/swoval/format/java/JavaFormatter.scala
@@ -10,8 +10,8 @@ import sbt.util.Logger
  * Formats a source path or verifies that the path is correctly formatted using
  * [[https://github.com/google/google-java-format google java formatter]].
  */
-object JavaFormatter extends ((Path, Path, Logger) => String) {
-  private val javaFormatter: (Path, Path, Logger) => String = {
+object JavaFormatter extends ((Path, Logger) => String) {
+  private val javaFormatter: (Path, Logger) => String = {
     val loader = this.getClass.getClassLoader match {
       case l: URLClassLoader =>
         val sorted = l.getURLs.toSeq.sortBy(u => if (u.toString.contains("guava")) -1 else 1)
@@ -22,20 +22,20 @@ object JavaFormatter extends ((Path, Path, Logger) => String) {
       .loadClass("com.swoval.format.java.JavaFormatterImpl$")
       .getDeclaredField("MODULE$")
       .get(null)
-  }.asInstanceOf[(Path, Path, Logger) => String]
+  }.asInstanceOf[(Path, Logger) => String]
 
   /**
    * Format the path using [[https://github.com/google/google-java-format google java formatter]].
    * @param path the path to format
    * @return true if the path is correctly formatted.
    */
-  def apply(unused: Path, path: Path, logger: Logger): String = javaFormatter(unused, path, logger)
+  def apply(path: Path, logger: Logger): String = javaFormatter(path, logger)
   override def toString = "JavaFormatter"
 }
 
-private[format] object JavaFormatterImpl extends ((Path, Path, Logger) => String) {
+private[format] object JavaFormatterImpl extends ((Path, Logger) => String) {
   private val formatter = new Formatter()
-  def apply(unused: Path, path: Path, logger: Logger): String =
+  def apply(path: Path, logger: Logger): String =
     try {
       val original = new String(Files.readAllBytes(path))
       formatter.formatSource(original)
---
+++ src/sbt-test/sbt-source-format/custom/project/build.sbt
@@ -0,0 +1,1 @@
+libraryDependencies += "com.swoval" %% "sbt-source-format-lib" % "0.2.3-SNAPSHOT"
